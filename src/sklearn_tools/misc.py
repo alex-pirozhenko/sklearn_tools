2c1137939d357524aef402341203d6cf457d2e11
alex-pirozhenko <ap@idamob.ru>
2014-11-13 17:42:19 -0500

    parameters support for json decoder

--- src/sklearn_tools/misc.py
+++ src/sklearn_tools/misc.py
@@ -4,6 +4,7 @@ from sklearn.base import TransformerMixin, BaseEstimator
 from functools import partial
 import scipy as sp
 import scipy.sparse
+import numpy as np
 
 
 class NoOpTransformer(object):
@@ -14,17 +15,16 @@ class NoOpTransformer(object):
         return X
 
 
-class JSONToObjectTransformer(NoOpTransformer, TransformerMixin):
+class JSONToObjectTransformer(BaseEstimator, TransformerMixin):
     """
     Assuming that X is an iterable over strings with JSON, this transformer parses the data
     """
-    def __init__(self, lazy=True, **kwargs):
+    def __init__(self, lazy=True):
         """
         :param kwargs: parameters for json.loads
         :return:
         """
-        self.kwargs = kwargs
-        self.func = partial(imap if lazy else map, partial(json.loads, **self.kwargs))
+        self.func = partial(imap if lazy else map, partial(json.loads))
 
     def transform(self, X):
         return self.func(X)
@@ -38,3 +38,14 @@ class RowNormalizer(BaseEstimator, TransformerMixin):
         normalizer = sp.sparse.lil_matrix((X.shape[0], X.shape[0]))
         normalizer.setdiag(1.0/X.sum(1))
         return normalizer * X
+
+
+class GBRTInitialEstimator(BaseEstimator, TransformerMixin):
+    def __init__(self, est):
+        self.est = est
+
+    def predict(self, X):
+        return self.est.predict_proba(X)[:, 1][:, np.newaxis]
+
+    def fit(self, X, y):
+        self.est.fit(X, y)
